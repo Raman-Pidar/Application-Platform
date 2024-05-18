6ece6419c0ad1451f9ba78aec7da3a3c60f74cd8
Raman Pidar <pidarraman@gmail.com>
2024-05-19 00:58:07 +0530

    feat: grid structure for jd cards

--- job-platform/src/components/page/jobListing.js
+++ job-platform/src/components/page/jobListing.js
@@ -1,18 +1,20 @@
+import Grid from '@mui/material/Grid';
+
 import { JobCard } from './card/card'
 
 
 export const JobListPage=(prop)=>{
     const list = prop.jdList
-
+console.log("joblistpage" ,prop)
     return (
-        <div>
-          {list.map((jd)=>{
-            return(
-              <div>
-                <JobCard />
-              </div>
-            )
-          })}
-        </div>
+          <Grid container direction="row" justifyContent="center" alignItems="center" spacing={4} key="scxs">
+          {list && list.map((jd)=>
+              <Grid item xs={12} sm={6} md={4} key={jd.jdUid} >
+                <JobCard jd={jd}/>
+              </Grid>
+            
+          )}
+          </Grid>
+         
       );
 }
