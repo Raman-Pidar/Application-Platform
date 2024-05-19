fe11af036151d8b494ceda9360cb3fc51f9da5d5
Raman Pidar <pidarraman@gmail.com>
2024-05-19 13:14:14 +0530

    refactor: job listing via flex replacing MUI GRID

--- src/components/page/jobListing.js
+++ src/components/page/jobListing.js
@@ -1,20 +1,22 @@
 import Grid from '@mui/material/Grid';
-
-import { JobCard } from './card/card'
+import './jobListing.css';
+import { JobCard } from './card/card';
 
 
 export const JobListPage=(prop)=>{
     const list = prop.jdList
 console.log("joblistpage" ,prop)
     return (
-          <Grid container direction="row" justifyContent="center" alignItems="center" spacing={4} key="scxs">
-          {list && list.map((jd)=>
-              <Grid item xs={12} sm={6} md={4} key={jd.jdUid} >
-                <JobCard jd={jd}/>
-              </Grid>
-            
-          )}
-          </Grid>
+        //   <Grid container direction="row" justifyContent="center" alignItems="center" spacing={4} key="scxs">
+          <div className='flexContainer'>
+                {list && list.map((jd)=>
+                    // <Grid item xs={12} sm={6} md={4} key={jd.jdUid} >
+                        <JobCard jd={jd}/>
+                    //</Grid>
+                    
+                )}
+          </div>
+         // </Grid>
          
       );
 }
