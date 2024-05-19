d0b83e85bf4956b1bfb6748efc791cda5a6824f5
Raman Pidar <pidarraman@gmail.com>
2024-05-19 11:53:41 +0530

    Feat: Infinite scrolling fetch function for recursive calling Setting dispatch for redux store

--- src/App.js
+++ src/App.js
@@ -1,43 +1,104 @@
-import {  useState, useEffect, Fragment } from 'react';
+import {  useState, useEffect } from 'react';
 import './App.css';
 import { Filter } from './components/filter/filter';
 import { JobListPage } from './components/page/jobListing';
+import { useSelector,useDispatch } from 'react-redux';
+import { setFetchedData, setFilteredData } from './features/slice';
+import InfiniteScroll from "react-infinite-scroll-component";
+import { filterJobs } from './components/filter/filter';
 
 function App() {
 
-  const [data, setData] = useState(null);
+  const [hasMore, setHasMore] = useState(true);
+  const [index, setIndex] = useState(10);
 
-  useEffect(()=>{
+  const data = useSelector((state)=>(state.jdState))
+  const filteredData = useSelector((state)=>(state.filteredState))
+  const filters = useSelector((state)=>(state.filters))
 
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    
-    const body = JSON.stringify({
-     "limit": 10,
-     "offset": 0
-    });
-    
-    const requestOptions = {
-     method: "POST",
-     headers: myHeaders,
-     body
+  const dispatch = useDispatch();
+
+  // recursive fetch function for infinite scrolling
+   const fetchMoreData = async (filters) => {
+
+      try {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+  
+        const body = JSON.stringify({
+          "limit": 10,
+          "offset": index,
+        });
+  
+        const requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body
+        };
+  
+        const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
+        const res = await response.json();
+        
+        // setData(result);
+        const newData = {...data,jdList:[...data.jdList,...res.jdList]}
+        //set state for original fetched data
+        dispatch(setFetchedData(newData));
+        //set state for filtered data
+        const filterData = filterJobs(filters , newData.jdList)
+        dispatch(setFilteredData(filterData))
+        res.data.length > 0 ? setHasMore(true) : setHasMore(false);
+      } catch (error) {
+        console.error(error);
+      }
+
+    setIndex((prevIndex) => prevIndex + 10);
+    console.log("more data fetched")
+  };
+
+ 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+  
+        const body = JSON.stringify({
+          "limit": 10,
+          "offset": 0,
+        });
+  
+        const requestOptions = {
+          //this API does not allow for GET call 
+          method: "POST",
+          headers: myHeaders,
+          body
+        };
+  
+        const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
+        const result = await response.json();
+        console.log(result)
+        dispatch(setFetchedData(result))
+        dispatch(setFilteredData(result))
+      } catch (error) {
+        console.error(error);
+      }
     };
+    fetchData()}, []);
 
-    fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
-     .then((response) => response.json())
-     .then((result) => setData(result))
-     .catch((error) => console.error(error));
-    
-  },[])
-
-  console.log(data);
+  console.log('setData', data, filteredData);
 
   return (
-    <Fragment>
+    <InfiniteScroll
+      dataLength={filteredData.jdList.length}
+      next={()=>fetchMoreData(filters)}
+      hasMore= {hasMore} >
+    <div className='app'>
       <Filter />
-      {data && <JobListPage jdList={data.jdList}/>}
-    </Fragment>
-      
+      {filteredData  && <JobListPage jdList={filteredData.jdList}/>}
+    </div> 
+    </InfiniteScroll>
+         
   );
 }
 
